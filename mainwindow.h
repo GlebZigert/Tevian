719a6b88c377e1245b792bbef123f602a4fc1831
GlebZigert <gleb.zigert@yandex.ru>
2022-09-14 16:19:01 +0300

    first commit

---
+++ mainwindow.h
@@ -0,0 +1,33 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include "requester/requester.h"
+#include <QtNetwork/QNetworkReply>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    Requester requester;
+
+private:
+    Ui::MainWindow *ui;
+    QGraphicsScene* m_graphicsScene;
+
+private slots:
+    void onPressLoadImage();
+    void onPressSaveImage();
+    void onPressFitWindow();
+    void onfinish(QNetworkReply *rep);
+};
+
+#endif // MAINWINDOW_H
